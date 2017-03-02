812d86fda1b6a5ad37f1d945db186329e519de7b
Maxime Rossi Bellom <mrossibellom@gmail.com>
2017-03-02 18:03:48 +0100

    Use only one root call to autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-SUBDIRS="armadito-core ui/systray-ui modules/mod-clamav modules/mod-pdf modules/mod-yara"
-
-for d in $SUBDIRS ; do
-    ( cd $d ; autoreconf --verbose --force --install )
-done
-
 # don't forget to do it in the toplevel directory
 autoreconf --verbose --force --install
 
