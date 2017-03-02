29a4613291e45c2eff9a048af742c2053d257440
François Déchelle <fdechelle@teclib.com>
2017-03-02 16:58:40 +0100

    Fix compilation without pkg-config Renamed core to armadito-core

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-SUBDIRS=core ui/systray-ui modules/mod-clamav modules/mod-pdf modules/mod-yara
+SUBDIRS="armadito-core ui/systray-ui modules/mod-clamav modules/mod-pdf modules/mod-yara"
 
 for d in $SUBDIRS ; do
     ( cd $d ; autoreconf --verbose --force --install )
 done
+
+# don't forget to do it in the toplevel directory
+autoreconf --verbose --force --install
 
